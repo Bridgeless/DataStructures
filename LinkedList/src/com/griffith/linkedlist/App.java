d20a8dda678a003d468585ade0284e406b9b6dce
U-HP_Laptop\Gareth <garethhgriffith@gmail.com>
2016-06-05 14:35:51 -0400

    Refactoring LinkedList

--- LinkedList/src/com/griffith/linkedlist/App.java
+++ LinkedList/src/com/griffith/linkedlist/App.java
@@ -2,26 +2,35 @@ package com.griffith.linkedlist;
 
 public class App {
 
-	public static SinglyLinkedList testList;
+	public static SinglyLinkedList<Integer> testList;
 	
 	public static void main(String[] args) {
 		
-		testList = new SinglyLinkedList();
-		testList.add(0);
+		testList = new SinglyLinkedList<Integer>();
+		testList.add(5);
+		testList.add(2);
+		testList.add(3);
+		testList.setter(0, 9);
+//		System.out.println(testList.set(0, 9));
+//		System.out.println(testList.get(0).next);
 //		testList.add(1);
 //		testList.add(2);
 //		testList.add(3);
+//		System.out.println(testList.remove(1) + " ====");
 //		testList.add(4);
-//		testList.add(0, 0);
-		
-		System.out.println(testList.size());
-		System.out.println(testList.get(0));
-		
+//		testList.add(0, 5);
+//		int test = testList.get(2);
+//		String testStr = Integer.toString(test);
+//		System.out.println(testStr);
+//		
+//		System.out.println(testList.size());
+//		System.out.println(testList.get(0));
+//		
 		for(int i = 0; i < testList.size(); i++)
 		{
-			System.out.print(testList.get(i));
+			System.out.print(testList.get(i) + " ");
 		}
-		
+//		
 	}
 
 }
--- LinkedList/src/com/griffith/linkedlist/SinglyLinkedList.java
+++ LinkedList/src/com/griffith/linkedlist/SinglyLinkedList.java
@@ -1,126 +1,118 @@
 package com.griffith.linkedlist;
 
-public class SinglyLinkedList {
-	// keeps track of the number of nodes in the list
-	private static int counter;
-	// keeps track of the beginning of the list
-	private Node head;
+public class SinglyLinkedList<E> {
 
-	public SinglyLinkedList() {
+	// public SinglyLinkedList() {
+	// }
+
+	private static class Node<E> {
+		// The data in the current node
+		private E data;
+		// The reference to the next node
+		private Node<E> next;
+
+		public Node(E data) {
+			this(data, null);
+		}
+
+		// Alternative constructor to use in case we want to set the nodes next
+		// value
+		public Node(E data, Node<E> next) {
+			this.data = data;
+			this.next = next;
+		}
 	}
+
+	// keeps track of the beginning of the list
+	private Node<E> head = null;
+	// keeps track of the number of nodes in the list
+	private int size;
 
 	// Appends the passed data to the end of the list
-	public void add(Object data) {
-		if (head == null) {
-			head = new Node(data);
-		}
-
-		Node tempNode = new Node(data);
-		Node currentNode = head;
-
-		if (currentNode != null) {
-			while (currentNode.getNext() != null) {
-				currentNode = currentNode.getNext();
-			}
-			currentNode.setNext(tempNode);
-		}
-		incrementCounter();
+	public boolean add(E data) {
+		add(size, data);
+		return true;
 	}
+
 	// Add an element at a specific spot in the list
-	public void add(Object data, int index)
-	{
-		Node tempNode = new Node(data);
-		Node currentNode = head;
-		// Check for null first before starting to traverse the list
-		if(currentNode != null)
-		{
-			//Crawl over the list until you find the requested insertion spot or the end of the list
-			for(int i = 0; i < index && currentNode.getNext() != null; i++)
-				currentNode = currentNode.getNext();
+	public void add(int index, E data) {
+		if (index < 0 || index > size) {
+			throw new IndexOutOfBoundsException(Integer.toString(index));
 		}
-		tempNode.setNext(currentNode.getNext());
-		currentNode.setNext(tempNode);
-		incrementCounter();
+		if (index == 0) {
+			head = new Node<E>(data, head);
+			size++;
+		} 
+		else {
+			Node<E> node = getNode(index - 1);
+			node.next = new Node<E>(data, node.next);
+			size++;
+		}
+	}
+
+	private Node<E> getNode(int index) {
+		Node<E> node = head;
+		for (int i = 0; i < index && node != null; i++) {
+			node = node.next;
+		}
+		return node;
+	}
+	
+	public E get(int index) {
+		if (index < 0 || index >= size) {
+			throw new IndexOutOfBoundsException(Integer.toString(index));
+		}
+		Node<E> node = getNode(index);
+		return node.data;
+	}
+
+	public E set(int index, E newValue) {
+		if (index < 0 || index >= size) {
+			throw new IndexOutOfBoundsException(Integer.toString(index));
+		}
+		Node<E> node = getNode(index);
+		E result = node.data;
+		node.data = newValue;
+		return result;
+	}
+	// This is really all that is needed to set a value
+	public void setter(int index, E newValue)
+	{
+		getNode(index).data = newValue;
+	}
+
+	public E remove(int index) {
+		if (index < 0 || index >= size) {
+			throw new IndexOutOfBoundsException(Integer.toString(index));
+		}
+		Node<E> node = getNode(index - 1);
+		Node<E> temp = node.next;
+		if (index == 0) {
+			if (head != null) {
+				head = head.next;
+			}
+			if (temp != null) {
+				size--;
+				return temp.data;
+			} else {
+				return null;
+			}
+		} 
+		else {			
+			if (temp != null) {
+				node.next = temp.next;
+				size--;
+				return temp.data;
+			} else {
+				return null;
+			}
+
+		}
 	}
 
 	public int size() {
-		return getCounter();
+		return size;
 	}
 
-	private int getCounter() {
-		return counter;
-	}
 
-	private void incrementCounter() {
-		counter++;
-	}
-
-	private void decrementCounter() {
-		counter--;
-	}
-
-	public Object get(int index) {
-		if (index <= 0) {
-			return null;
-		}
-		Node currentNode = null;
-		if (head != null) {
-			currentNode = head.getNext();
-			for (int i = 0; i < index; i++) {
-				if (currentNode.getNext() == null) {
-					return null;
-				}
-				currentNode = currentNode.getNext();
-			}
-			return currentNode.getData();
-		}
-		return currentNode;
-	}
-	
-//	public boolean remove(int index)
-//	{
-//		if(index < 1 || index >size())
-//		{
-//			return false;
-//		}
-//		
-//		
-//		
-//	}
-
-	private class Node {
-		// The reference to the next node
-		Node next;
-		// The data in the current node
-		Object data;
-
-		public Node(Object dataValue) {
-			next = null;
-			data = dataValue;
-		}
-
-		// Alternative constructor to use in case we want to set the nodes next value
-		@SuppressWarnings("unused")
-		public Node(Object dataValue, Node nextValue) {
-			data = dataValue;
-			next = nextValue;
-		}
-
-		public Object getData() {
-			return data;
-		}
-
-		@SuppressWarnings("unused")
-		public void setData(Object dataValue) {
-			data = dataValue;
-		}
-
-		public Node getNext() {
-			return next;
-		}
-
-		public void setNext(Node nextValue) {
-			next = nextValue;
-		}
-	}
 }
